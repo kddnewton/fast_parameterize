c15d7f32cae2004b7a6a95a2dace11a1bfae5697
Kevin Deisz <kevin.deisz@gmail.com>
2021-04-16 12:40:29 -0400

    Get the parameterization working

--- lib/fast_parameterize.rb
+++ lib/fast_parameterize.rb
@@ -3,35 +3,58 @@
 require 'fast_parameterize/version'
 require 'fast_parameterize/fast_parameterize'
 
+# By default, FastParameterize already defines String#parameterize and
+# FastParameterize::parameterize. In the case that we're using ActiveSupport,
+# however, there is the additional functionality of transliteration that we need
+# to account for. In this case, we're going to first allow ActiveSupport to do
+# that work, then we're going to do the actual parameterization.
 module FastParameterize
-  def self.install
-    ActiveSupport::Inflector.singleton_class.then do |clazz|
-      clazz.alias_method(:as_parameterize, :parameterize)
-      clazz.prepend(FastParameterize)
+  # Override ActiveSupport::Inflector::parameterize to use
+  # FastParameterize::parameterize.
+  module ActiveSupportInflectorPatch
+    def parameterize(string, separator: "-", preserve_case: false, locale: nil)
+      FastParameterize.parameterize(
+        ActiveSupport::Inflector.transliterate(string, locale: locale),
+        separator: separator,
+        preserve_case: preserve_case
+      )
     end
+  end
+
+  # Override String#parameterize to use FastParameterize::parameterize.
+  module ActiveSupportStringPatch
+    def parameterize(separator: "-", preserve_case: false, locale: nil)
+      FastParameterize.parameterize(
+        ActiveSupport::Inflector.transliterate(self, locale: locale),
+        separator: separator,
+        preserve_case: preserve_case
+      )
+    end
+  end
+
+  # Override ActiveSupport::Inflector::method_added so that if and when the
+  # parameterize method gets defined, we can immediately redefine it.
+  module ActiveSupportDelayedPatch
+    def method_added(method)
+      FastParameterize.active_support if method == :parameterize
+      super
+    end
+  end
+
+  # Hook into ActiveSupport::Inflector and String to take advantage of
+  # FastParameterize.
+  def self.active_support
+    ActiveSupport::Inflector.singleton_class.prepend(ActiveSupportInflectorPatch)
+    String.prepend(ActiveSupportStringPatch)
   end
 end
 
 if defined?(ActiveSupport)
-  FastParameterize.install
+  FastParameterize.active_support
 else
   module ActiveSupport
     module Inflector
-      class << self
-        prepend(
-          Module.new do
-            # Hooks into ActiveSupport::Inflector and waits for the
-            # #parameterize method to be defined. When it is, it automatically
-            # redefines it. Using this `prepend` trick to attempt to be a good
-            # citizen in the case that someone else has already hooked into
-            # `method_added` on `Inflector`.
-            def method_added(method)
-              FastParameterize.install if method == :parameterize
-              super
-            end
-          end
-        )
-      end
+      prepend(FastParameterize::ActiveSupportDelayedPatch)
     end
   end
 end
--- test/fast_parameterize_test.rb
+++ test/fast_parameterize_test.rb
@@ -3,6 +3,19 @@
 require 'test_helper'
 
 class FastParameterizeTest < Minitest::Test
+  def test_basic
+    assert_equal 'foo-bar-baz', '^FOO@BAR?BAZ$'.parameterize
+  end
+
+  def test_separator
+    assert_equal 'foo_bar_baz', '^FOO@BAR?BAZ$'.parameterize(separator: '_')
+    assert_equal 'foo..bar..baz', '^FOO@BAR?BAZ$'.parameterize(separator: '..')
+  end
+
+  def test_preserve_case
+    assert_equal 'FOO-BAR-BAZ', '^FOO@BAR?BAZ$'.parameterize(preserve_case: true)
+  end
+
   def test_version
     refute_nil ::FastParameterize::VERSION
   end
